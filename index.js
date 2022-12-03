334fad26456b8bdad7a6d4d8d0a635d68c3374a3
Olga Mishareva <mishareva.olga@gmail.com>
2022-12-03 22:27:58 +0100

    feat: add diverse colors of cards

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const cardColors = [
+  "#edb5bf", // rosa
+  "#fcbd96", // peach
+  "#9fcce4", // light blue
+  "#9fedd7", // light green
+  "#fce181", // light yellow
+];
+
 const from = document.querySelector('[data-js="form"]');
 const cardBox = document.querySelector('[data-js="cardbox"]');
 
@@ -7,8 +15,16 @@ function getTemplate() {
   return cardElement;
 }
 
+function handleCardColor() {
+  let index = Math.random() * 10;
+  index = index > 5 ? Math.floor(index / 2) : Math.floor(index - 1);
+  return cardColors[index];
+}
+
 function createCard(question, answer, tags) {
   const newCard = getTemplate();
+  newCard.querySelector('[data-js="card"]').style.backgroundColor =
+    handleCardColor();
   const cardQuestion = newCard.querySelector('[data-js="question"]');
   const answerButton = newCard.querySelector('[data-js="answer-btn"]');
   const cardAnswer = newCard.querySelector('[data-js="answer"]');
@@ -43,14 +59,15 @@ function handleBookmarkButton(newCard) {
 function handleTags(newCard, tags) {
   const tagsBox = newCard.querySelector('[data-js="tags"]');
   const tagsList = tags.split(",");
-  console.log(tagsList);
 
-  tagsList.forEach((item) => {
-    const tag = document.createElement("li");
-    tag.classList.add("card__tag");
-    tag.textContent = item;
-    tagsBox.append(tag);
-  });
+  if (tagsList[0] !== "") {
+    tagsList.forEach((item) => {
+      const tag = document.createElement("li");
+      tag.classList.add("card__tag");
+      tag.textContent = item;
+      tagsBox.append(tag);
+    });
+  }
 }
 
 from.addEventListener("submit", (event) => {
